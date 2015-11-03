d5e7bb5d3cc855aa2e32a87234b66c3b139b7d01
Rodrigo <camachorod@gmail.com>
2015-11-02 22:40:34 -0500

    Just added a comment to see the difference as shown by git#

--- python/allergies/allergies.py
+++ python/allergies/allergies.py
@@ -16,9 +16,10 @@ class Allergies:
         self.score = score
 
     def is_allergic_to(self, allergy):
-        return self.score & 1 << self._allergies.index(allergy)
+        return self.score & 1 << self._allergies.index(allergy) #this is a comment
 
     @property
     def lst(self):
         return [allergy for allergy in self._allergies
                 if self.is_allergic_to(allergy)]
+    

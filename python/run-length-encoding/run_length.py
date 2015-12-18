ec7d66ab1bd71d0ea4a4c993992a11db83263715
Rodrigo <camachorod@gmail.com>
2015-12-18 18:31:22 +0100

    finished the run-time exercise

--- python/run-length-encoding/run_length.py
+++ python/run-length-encoding/run_length.py
@@ -1,13 +1,37 @@
+import re
+
 def encode (in_string):
-    output_dict = {}
-    unique_string = set(in_string)
-    for char in unique_string:
-        output_dict[char] = in_string.count(char)
-    return output_dict
+    counter = 1
+    prev_char = ""
+    out_string = ""
+    for char in in_string:
+        if char != prev_char:
+            if prev_char: 
+                if counter == 1:   #This if/else has to be here since the out_strings expected don't have "1"'s in front if there is only one of the char in question
+                    out_string += prev_char
+                else:
+                    out_string += str(counter) + prev_char
+            counter = 1
+            prev_char = char
+        else: 
+            counter += 1
+    else:
+        if counter == 1:
+            out_string += char
+        else: 
+            out_string += str(counter) + char
+    return out_string
     
     
 def decode (in_string): 
-    sdfj
-    
-    
-print encode("hello this is rodrigo")
+    out_string = ""
+    multiplier = ""
+    for char in in_string: 
+        if char.isdigit():
+            multiplier += char
+        elif multiplier: #extra case had to be made for chars only shown once in a row
+            out_string += int(multiplier)*char
+            multiplier = ""
+        else:
+            out_string += char
+    return out_string
